4c1d2231baab8dd6daa52e0ae1a616dab16c858a
Farothtc <mvkirimi@gmail.com>
2024-12-31 16:12:52 +0300

    Initial commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,13 @@
 import "./globals.css";
+import { Nav } from "./components/Nav";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
